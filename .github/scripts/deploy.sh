b780ac079080ec63625c50a9326771572fa66ca3
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-07 22:48:41 -0500

    fix test deployment: invalid working directory

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -6,6 +6,11 @@ echo "$DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
 
 chmod 600 ~/.ssh/id_rsa
 
+tmp=$(pwd)
 make build
+cd tmp 
+
+pwd
+ls -alh
 
 rsync -av --delete -e "ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" ./build root@78.47.67.22:/apinator
