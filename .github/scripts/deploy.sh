cbc7763f8c598f37afed02068f0fb4e1383de6b3
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-07 22:44:23 -0500

    test deployment

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+echo "$DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
+
+chmod 600 ~/.ssh/id_rsa
+
+make build
+
+rsync -av --delete -e "ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa" ./build root@78.47.67.22:/apinator
