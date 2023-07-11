f21c3d616a86d2f06d8f0ac328db5c9f17db4c37
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-11 17:21:55 -0500

    some more css

--- web/js/index.js
+++ web/js/index.js
@@ -4,6 +4,8 @@ function GlobalInit() {
         language: 'javascript'
     });
 
+    setupFilesystem();
+
     // if (localStorage.getItem("X-APINATOR-AUTH") == "") {
     //     document.querySelector(".signinpopup").style.display = "block";
     //     logininit();
@@ -24,5 +26,38 @@ function logininit() {
         localStorage.setItem("X-APINATOR-AUTH", value);
 
         document.querySelector(".signinpopup").style.display = "none";
+    });
+}
+
+function setupFilesystem() {
+    var hierarchy = document.getElementById("hierarchy");
+    hierarchy.addEventListener("click", function (event) {
+        var elem = event.target;
+        if (elem.tagName.toLowerCase() == "span" && elem !== event.currentTarget) {
+            var type = elem.classList.contains("folder") ? "folder" : "file";
+            if (type == "file") {
+                alert("File accessed");
+            }
+            if (type == "folder") {
+                var isexpanded = elem.dataset.isexpanded == "true";
+                if (isexpanded) {
+                    elem.classList.remove("fa-folder-o");
+                    elem.classList.add("fa-folder");
+                }
+                else {
+                    elem.classList.remove("fa-folder");
+                    elem.classList.add("fa-folder-o");
+                }
+                elem.dataset.isexpanded = !isexpanded;
+
+                var toggleelems = [].slice.call(elem.parentElement.children);
+                var classnames = "file,foldercontainer,noitems".split(",");
+
+                toggleelems.forEach(function (element) {
+                    if (classnames.some(function (val) { return element.classList.contains(val); }))
+                        element.style.display = isexpanded ? "none" : "block";
+                });
+            }
+        }
     });
 }
